77aecde5d0563dab32f28b83f37efa372ff7eaad
Jordan Villacorta <jvillacortadla@gmail.com>
2021-09-29 12:35:18 -0400

    Update schema according to csv file data.

--- schema.sql
+++ schema.sql
@@ -5,33 +5,24 @@ CREATE DATABASE product_overview;
 -- Products
 
 CREATE TABLE IF NOT EXISTS products (
-  product_id INT NOT NULL,
+  product_id INT SERIAL,
   name VARCHAR(100),
-  slogan VARCHAR(100),
-  description VARCHAR(200),
-  category VARCHAR(50),
-  default_price VARCHAR(50),
+  slogan TEXT,
+  description TEXT,
+  category VARCHAR(100),
+  default_price VARCHAR(100),
   PRIMARY KEY(product_id)
 );
 
-CREATE TABLE IF NOT EXISTS product_features (
-  feature_id INT NOT NULL,
-  product_id INT NOT NULL,
-  PRIMARY KEY(feature_id),
-  CONSTRAINT fk_product
-    FOREIGN KEY(feature_id)
-      REFERENCES products(product_id)
-);
-
 CREATE TABLE IF NOT EXISTS features (
-  id INT NOT NULL,
-  feature_id INT NOT NULL,
+  feature_id INT SERIAL,
+  product_id INT NOT NULL,
   feature VARCHAR(100),
   value VARCHAR(100),
-  PRIMARY KEY(id),
-  CONSTRAINT fk_product_features
-    FOREIGN KEY(feature_id)
-      REFERENCES product_features(feature_id)
+  PRIMARY KEY(feature_id),
+  CONSTRAINT fk_product_id
+    FOREIGN KEY(product_id)
+      REFERENCES products(product_id)
 );
 
 CREATE TABLE IF NOT EXISTS related_products (
@@ -46,10 +37,10 @@ CREATE TABLE IF NOT EXISTS related_products (
 -- Styles
 
 CREATE TABLE IF NOT EXISTS product_styles (
-  style_id INT NOT NULL,
+  style_id INT SERIAL,
   product_id INT NOT NULL,
-  name VARCHAR(100),
-  slogan VARCHAR(100),
+  name VARCHAR(200),
+  slogan TEXT,
   original_price VARCHAR(50),
   sale_price VARCHAR(50),
   style_default BOOLEAN NOT NULL,
@@ -60,10 +51,10 @@ CREATE TABLE IF NOT EXISTS product_styles (
 );
 
 CREATE TABLE IF NOT EXISTS style_photos (
-  photos_id INT NOT NULL,
+  photos_id INT SERIAL,
   style_id INT NOT NULL,
-  thumbnail_url VARCHAR(100),
-  url VARCHAR(100),
+  url TEXT,
+  thumbnail_url TEXT,
   PRIMARY KEY(photos_id),
   CONSTRAINT fk_style_id
     FOREIGN KEY(style_id)
@@ -73,10 +64,10 @@ CREATE TABLE IF NOT EXISTS style_photos (
 -- Skus
 
 CREATE TABLE IF NOT EXISTS style_skus (
-  sku_id INT NOT NULL,
-  quantity INT,
-  size VARCHAR(50),
+  sku_id INT SERIAL,
   style_id INT NOT NULL,
+  size VARCHAR(50),
+  quantity INT,
   PRIMARY KEY(sku_id),
   CONSTRAINT fk_style_id
     FOREIGN KEY(style_id)
@@ -84,12 +75,13 @@ CREATE TABLE IF NOT EXISTS style_skus (
 );
 
 CREATE TABLE IF NOT EXISTS cart (
-  cart_id INT NOT NULL,
-  sku_id INT NOT NULL,
-  count INT,
+  cart_id INT SERIAL,
+  user_session INT,
+  product_id INT NOT NULL,
+  active INT,
   PRIMARY KEY(cart_id),
-  CONSTRAINT fk_sku_id
-    FOREIGN KEY(sku_id)
-      REFERENCES style_skus(sku_id)
+  CONSTRAINT fk_product_id
+    FOREIGN KEY(product_id)
+      REFERENCES products(product_id)
 );
 
