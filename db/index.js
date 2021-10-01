1018208a289b54b815b35320468a2e5202cadacb
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-01 02:16:17 -0400

    Create queries that join features array table with singleProduct query. Query successful.

--- db/index.js
+++ db/index.js
@@ -27,16 +27,37 @@ const getAllProducts = (req, res) => {
 }
 
 const getSingleProduct = (req, res) => {
+  // NEED TO JOIN/MERGE FEATURES TABLE
   const productId = req.params.product_id;
-  pool.query('SELECT * FROM products WHERE product_id = $1', [productId], (err, data) => {
+  console.log(productId);
+  // pool.query(`SELECT * FROM products WHERE products.id = ${productId}`, (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //     throw err;
+  //   } else {
+  //     console.log(data.rows[0]);
+  //     res.status(200).json(data.rows[0]);
+  //   }
+  // });
+  // subquery: what I want to aggregate
+  pool.query(`SELECT * FROM products WHERE products.id = ${productId}`, (err, data) => {
     if (err) {
       console.log(err);
       throw err;
     } else {
-      console.log(data.rows[0]);
-      res.status(200).json(data.rows[0]);
+      const fQuery = `SELECT feature, value FROM features WHERE features.feature_id=${productId}`;
+      const productInfo = data.rows;
+      pool.query(fQuery, (err, data) => {
+        if (err) {
+          console.log(err);
+          throw err;
+        } else {
+          productInfo[0].features = data.rows;
+          res.status(200).json(productInfo);
+        }
+      })
     }
-  });
+  })
 }
 
 const getProductStyles = (req, res) => {
