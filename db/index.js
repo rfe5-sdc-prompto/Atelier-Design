f93edabced768a145cdecdf0ddd283f432f91751
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-08 15:05:08 -0400

    Change pool connection for future server instances.

--- db/index.js
+++ db/index.js
@@ -12,8 +12,12 @@ pool.connect((err, data) => {
   if (err) {
     return console.error('Error acquiring client', err.stack)
   } else {
-    console.log('Database is now connected!');
+    let dbConnected = pool.query('SELECT * FROM products WHERE products.id=1').then(data => {
+      console.log(data.rows[0].id, 'Database is now connected!');
+    })
+    return dbConnected;
   }
+
 });
 
 const getAllProducts = (req, res) => {
